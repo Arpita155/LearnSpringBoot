4de6659af16069856ad2623ba6175e397cb7f935
Arpita Sahoo <arpitasahoo795@gmail.com>
2026-02-07 00:14:19 +0530

    msg

--- main/java/com/example/demo/controller/HelloController.java
+++ main/java/com/example/demo/controller/HelloController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,4 +22,34 @@ public class HelloController {
     public String ifo(){
         return "I am Arpita and i am learning spring boot";
     }
+
+    // PathVariable
+    @GetMapping("/hello/{name}")
+    public String sayHello(@PathVariable String name){
+        return "Hello "+name +" welcome to spring boot";
+    }
+
+    @GetMapping("/square/{num}")
+    public int Square(@PathVariable int num){
+        return num*num;
+    }
+
+    // @RequestParam (Query parameters)
+    @GetMapping("/greet")
+    public String greet(@RequestParam String name){
+        return "Good Morning"+ name;
+    }
+
+    @GetMapping("/sum")
+    public String sum(@RequestParam int a, @RequestParam int b){
+        int sum =  a+b;
+        return "Sum = "+sum;
+    }
+
+    // Optional RequestParam
+    @GetMapping("/Welcome")
+    public String Welcome(@RequestParam(defaultValue = "Guest") String name){
+        return "Welcome "+name;
+    }
+
 }
